71958f2e255885c18ca4b9b097bdc22502478f09
Robotwaterfall <jaysonwelshman@gmail.com>
2025-07-09 19:59:28 -0400

    The stepper motor now half-steps tohave more torque

--- Steeper-Motor-Testing/Steeper-Motor-Testing.ino
+++ Steeper-Motor-Testing/Steeper-Motor-Testing.ino
@@ -2,84 +2,42 @@
 #define STEPPER_PIN_2 10
 #define STEPPER_PIN_3 11
 #define STEPPER_PIN_4 12
-int step_number = 0;
-void setup() {
-pinMode(STEPPER_PIN_1, OUTPUT);
-pinMode(STEPPER_PIN_2, OUTPUT);
-pinMode(STEPPER_PIN_3, OUTPUT);
-pinMode(STEPPER_PIN_4, OUTPUT);
 
+int step_number = 0;
+
+// Half-step sequence (8 steps)
+const int stepSequence[8][4] = {
+  {HIGH, LOW,  LOW,  LOW},
+  {HIGH, HIGH, LOW,  LOW},
+  {LOW,  HIGH, LOW,  LOW},
+  {LOW,  HIGH, HIGH, LOW},
+  {LOW,  LOW,  HIGH, LOW},
+  {LOW,  LOW,  HIGH, HIGH},
+  {LOW,  LOW,  LOW,  HIGH},
+  {HIGH, LOW,  LOW,  HIGH}
+};
+
+void setup() {
+  pinMode(STEPPER_PIN_1, OUTPUT);
+  pinMode(STEPPER_PIN_2, OUTPUT);
+  pinMode(STEPPER_PIN_3, OUTPUT);
+  pinMode(STEPPER_PIN_4, OUTPUT);
 }
 
 void loop() {
- 
-  OneStep(true);
-  delay(2);
-  
-
+  OneStep(true);  // true = forward, false = backward
+  delay(2);       // Adjust this delay to change speed
 }
 
+void OneStep(bool dir) {
+  if (dir) step_number++;
+  else     step_number--;
 
-void OneStep(bool dir){
-    if(dir){
-switch(step_number){
-  case 0:
-  digitalWrite(STEPPER_PIN_1, HIGH);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 1:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, HIGH);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 2:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, HIGH);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 3:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, HIGH);
-  break;
-} 
-  }else{
-    switch(step_number){
-  case 0:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, HIGH);
-  break;
-  case 1:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, HIGH);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 2:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, HIGH);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 3:
-  digitalWrite(STEPPER_PIN_1, HIGH);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
- 
-  
-} 
-  }
-step_number++;
-  if(step_number > 3){
-    step_number = 0;
-  }
+  if (step_number > 7) step_number = 0;
+  if (step_number < 0) step_number = 7;
+
+  digitalWrite(STEPPER_PIN_1, stepSequence[step_number][0]);
+  digitalWrite(STEPPER_PIN_2, stepSequence[step_number][1]);
+  digitalWrite(STEPPER_PIN_3, stepSequence[step_number][2]);
+  digitalWrite(STEPPER_PIN_4, stepSequence[step_number][3]);
 }
-
